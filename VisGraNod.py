5781719587645e354dea79dac1cb0258b2639e78
Jhonny Jiménez <MasterChampionLigue@gmail.com>
2022-10-24 06:11:55 -0600

    Implemetación del sistema de desplazamiento y zoom usados en el editor de nodos de Blender.

--- VisGraNod.py
+++ VisGraNod.py
@@ -13,6 +13,12 @@ class QDMGraphicsView(QGraphicsView):
 
         self.setScene(self.grScene)
 
+        self.zoomInFactor = 1.25
+        self.zoomClamp = True
+        self.zoom = 10
+        self.zoomStep = 1
+        self.zoomRange = [0, 10]
+
     def initUI(self):
         self.setRenderHints(QPainter.Antialiasing | QPainter.HighQualityAntialiasing | QPainter.TextAntialiasing | QPainter.SmoothPixmapTransform)
 
@@ -20,3 +26,75 @@ class QDMGraphicsView(QGraphicsView):
 
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonPress(event)
+        elif event.button() == Qt.LeftButton:
+            self.leftMouseButtonPress(event)
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonPress(event)
+        else:
+            super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonRelease(event)
+        elif event.button() == Qt.LeftButton:
+            self.leftMouseButtonRelease(event)
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonRelease(event)
+        else:
+            super().mouseReleaseEvent(event)
+
+    def middleMouseButtonPress(self, event):
+        releaseEvent = QMouseEvent(QEvent.MouseButtonRelease, event.localPos(), event.screenPos(),
+                                   Qt.LeftButton, Qt.NoButton, event.modifiers())
+        super().mouseReleaseEvent(releaseEvent)
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
+                                Qt.LeftButton, event.buttons() | Qt.LeftButton, event.modifiers())
+        super().mousePressEvent(fakeEvent)
+
+    def middleMouseButtonRelease(self, event):
+        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
+                                Qt.LeftButton, event.buttons() | Qt.LeftButton, event.modifiers())
+        super().mouseReleaseEvent(fakeEvent)
+        self.setDragMode(QGraphicsView.NoDrag)
+
+    def leftMouseButtonPress(self, event):
+        return super().mousePressEvent(event)
+
+    def leftMouseButtonRelease(self, event):
+        return super().mouseReleaseEvent(event)
+
+    def rightMouseButtonPress(self, event):
+        return super().mousePressEvent(event)
+
+    def rightMouseButtonRelease(self, event):
+        return super().mouseReleaseEvent(event)
+
+    def wheelEvent(self, event):
+        #Cálculo del factor de zoom.
+        zoomOutFactor = 1 / self.zoomInFactor
+
+        #Cálculo del zoom
+        if event.angleDelta().y() > 0:
+            zoomFactor = self.zoomInFactor
+            self.zoom += self.zoomStep
+        else:
+            zoomFactor = zoomOutFactor
+            self.zoom -= self.zoomStep
+
+        clamped = False
+        if self.zoom < self.zoomRange[0]:
+            self.zoom, clamped = self.zoomRange[0], True
+        if self.zoom > self.zoomRange[1]:
+            self.zoom, clamped = self.zoomRange[1], True
+
+        # Configuración de la escala de la escena.
+        if not clamped or self.zoomClamp is False:
+            self.scale(zoomFactor, zoomFactor)
